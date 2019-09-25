9c372b2129e87b56b9b05edee9d0df4b2d7c6743
Obrien  Aidan M <obrienam@student2.cs.appstate.edu>
2019-09-25 14:20:58 -0400

    Started frame code

--- Edge Detection/frames.py
+++ Edge Detection/frames.py
@@ -1,1 +1,13 @@
-print("Blah")
+import cv2
+
+def FrameCaputre(path):
+    vidObj = cv2.VideoCapture(path)
+    count = 0
+    success = 1
+    while success:
+        success, image = vidObj.read()
+        cv2.imwrite("frame%d.jpg" % count, image)
+        if __name__ == '__main__': 
+  
+    # Calling the function 
+    FrameCapture("C:\\Users\\Admin\\PycharmProjects\\project_1\\openCV.mp4") 
