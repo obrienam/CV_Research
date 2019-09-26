f6cfa058f4f09e4335fcbfce2134f467be39a9df
Aidan O'Brien <obrienam@appstate.edu>
2019-09-25 20:25:09 -0400

    Rewrote frames.py and generated some images.

--- Edge Detection/frames.py
+++ Edge Detection/frames.py
@@ -1,12 +1,17 @@
 import cv2
-
-def FrameCapture(path):
-    vidObj = cv2.VideoCapture(path)
-    count = 0
-    success = 1
-    while success:
-        success, image = vidObj.read()
-        cv2.imwrite("frame%d.jpg" % count, image)
-if __name__ == '__main__': 
-    # Calling the function 
-    FrameCapture("Documents/GitHub/CV_Research/Edge Detection/rpi22@2016-06-09@17-02-14BirdAndBees.mp4") 
+vidcap = cv2.VideoCapture('/Users/aidanobrien/Documents/GitHub/CV_Research/Edge Detection/rpi22@2016-06-09@17-02-14BirdAndBees.mp4')
+def getFrame(sec):
+    vidcap.set(cv2.CAP_PROP_POS_MSEC,sec*1000)
+    hasFrames,image = vidcap.read()
+    if hasFrames:
+        cv2.imwrite("image"+str(count)+".jpg", image)     # save frame as JPG file
+    return hasFrames
+sec = 0
+frameRate = 0.5 #//it will capture image in each 0.5 second
+count=1
+success = getFrame(sec)
+while success:
+    count = count + 1
+    sec = sec + frameRate
+    sec = round(sec, 2)
+    success = getFrame(sec)
