e85ab36556baf16212cf56d891ccf5b3a5e97962
Aidan O'Brien <obrienam@appstate.edu>
2019-10-02 15:10:22 -0400

    Worked on reducing noise in edge detector

--- Edge Detection/EdgeSlider.py
+++ Edge Detection/EdgeSlider.py
@@ -5,17 +5,17 @@ import sys
 max_lowThreshold = 100
 window_name = sys.argv[2]
 title_trackbar = 'Min Threshold:'
-ratio = 1
+ratio = 2
 kernel_size = 3
 def CannyThreshold(val):
     low_threshold = val
-    img_blur = cv.blur(src_gray, (3,3))
-    detected_edges = cv.Canny(img_blur, low_threshold, low_threshold*ratio, kernel_size)
+    img_blur = cv.blur(src_gray, (3,3), 0)
+    detected_edges = cv.Canny(img_blur, low_threshold, low_threshold*ratio)
     mask = detected_edges != 0
     dst = src * (mask[:,:,None].astype(src.dtype))
-    cv.imshow(window_name, dst)
+    cv.imshow(window_name, detected_edges)
 parser = argparse.ArgumentParser(description='Code for Canny Edge Detector tutorial.')
-parser.add_argument('--input', help='Path to input image.', default=sys.argv[1])
+parser.add_argument('--input', help='Path to input image.', default=sys.argv[2])
 args = parser.parse_args()
 src = cv.imread(cv.samples.findFile(args.input))
 if src is None:
