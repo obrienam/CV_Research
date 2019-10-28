eb7d0c133b862681f0d0d9f16e5ce23b04b77a87
Aidan O'Brien <obrienam@appstate.edu>
2019-10-28 14:23:34 -0400

    Cleaned up project a bit

--- Edge Detection/sift.py
+++ Edge Detection/sift.py
@@ -2,29 +2,23 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 
-img1 = cv2.imread('test/image287contrastedge.jpg',0)          # queryImage
-img2 = cv2.imread('test/image288contrastedge.jpg',0) # trainImage
-
-# Initiate SIFT detector
+img1 = cv2.imread('../Assets/image287.jpg',0)  
+img2 = cv2.imread('../Assets/image288.jpg',0) 
 sift = cv2.xfeatures2d.SIFT_create()
 
-# find the keypoints and descriptors with SIFT
 kp1, des1 = sift.detectAndCompute(img1,None)
 kp2, des2 = sift.detectAndCompute(img2,None)
 
-# FLANN parameters
-FLANN_INDEX_KDTREE = 0
+FLANN_INDEX_KDTREE = 1
 index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
-search_params = dict(checks=50)   # or pass empty dictionary
+search_params = dict(checks=50)  
 
 flann = cv2.FlannBasedMatcher(index_params,search_params)
 
 matches = flann.knnMatch(des1,des2,k=2)
 
-# Need to draw only good matches, so create a mask
 matchesMask = [[0,0] for i in xrange(len(matches))]
 
-# ratio test as per Lowe's paper
 for i,(m,n) in enumerate(matches):
     if m.distance < 0.7*n.distance:
         matchesMask[i]=[1,0]
