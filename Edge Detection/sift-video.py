f23406cfb81f3c4e3d149ad74c0dc68661d56268
Aidan O'Brien <obrienam@appstate.edu>
2019-11-18 16:18:21 -0500

    Created initial sift code for bee videos.

--- Edge Detection/sift-video.py
+++ Edge Detection/sift-video.py
@@ -1,10 +1,22 @@
 import cv2
 import matplotlib.pyplot as plt
 import math
-
-def upContrast(img1,img2,)
+import numpy as np
+def upContrast(img1,img2,alpha,beta):
+    n_image1 = np.zeros(img1.shape, img1.dtype)
+    n_image2 = np.zeros(img2.shape, img2.dtype)
+    for y in range(img1.shape[0]):
+        for x in range(img1.shape[1]):
+            for c in range(img1.shape[2]):
+                n_image1[y,x,c]=np.clip(alpha*img1[y,x,c] + beta, 0, 255)
+    for y in range(img2.shape[0]):
+        for x in range(img2.shape[1]):
+            for c in range(img2.shape[2]):
+                n_image2[y,x,c]=np.clip(alpha*img2[y,x,c] + beta, 0, 255)
+    return n_image1,n_image2
 def countStill(img1, img2):
-    bk = cv2.imread('../Assets/bee-backgroundcontrast.png')
+    m=0
+    bk = cv2.imread('../Assets/bee-background.png')
     subImage1=(bk.astype('int32')-img1.astype('int32')).clip(0).astype('uint8')
     grey1=cv2.cvtColor(subImage1,cv2.COLOR_BGR2GRAY)
     retval1,thresh1=cv2.threshold(grey1,35,255,cv2.THRESH_BINARY_INV)
@@ -36,20 +48,28 @@ def countStill(img1, img2):
             #print(kp2[j].pt[1])
             
             if(abs(kp1[i].pt[0]-kp2[j].pt[0])<2 and abs(kp1[i].pt[1]-kp2[j].pt[1])<4):
-                print("match")
+                m+=1
                 break
         
     pt=keypoints_1[0].pt
+    return m
 
-vs=cv2.VideoCapture("/Users/aidanobrien/Documents/GitHub/CV_Research/Assets/bees2.mp4")
+vs=cv2.VideoCapture("C:/Users/obrienam/Documents/GitHub/CV_Research/Assets/bees2.mp4")
 firstFrame=None
+prev_s=None
 while True:
     hasFrames,frame=vs.read()
     if (hasFrames==False):
         break
     cv2.imshow("vid",frame)
     if firstFrame is not None:
-        
+        s=countStill(firstFrame,frame)
+        if(prev_s is None):
+            prev_s=s
+        if(prev_s<s):
+            prev_s=s
+            print(s)    
+    
     key=cv2.waitKey(1) & 0xFF
 
     if key == ord("q"):
